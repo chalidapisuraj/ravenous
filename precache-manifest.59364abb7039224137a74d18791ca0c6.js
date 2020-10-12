0f6e0025e8ff9a09fad1978fa4f5d5ccf103329e
Chalida Pisuraj <pisurajc@uci.edu>
2020-10-12 13:10:42 -0700

    Updates

---
+++ precache-manifest.59364abb7039224137a74d18791ca0c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5951dd1adbc6c0e02b56a85e15ec79",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "61e9e918a362bc74ef51",
+    "url": "/ravenous/static/css/main.deea4ecc.chunk.css"
+  },
+  {
+    "revision": "44b17a0c356c7e732238",
+    "url": "/ravenous/static/js/2.4f2af0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.4f2af0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e9e918a362bc74ef51",
+    "url": "/ravenous/static/js/main.d0cb83d3.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
